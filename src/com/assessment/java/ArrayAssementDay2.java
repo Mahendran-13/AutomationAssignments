e21e041b75f7a23a93eb20ed499debeb5baabc12
Mahendran <mahendranvsm13@gmail.com>
2024-12-23 21:41:03 +0530

    assignment2 Array

---
+++ src/com/assessment/java/ArrayAssementDay2.java
@@ -0,0 +1,40 @@
+package com.assessment.java;
+
+public class ArrayAssementDay2 {
+
+	public static void main(String[] args) {
+
+		//main category 1 
+		String [] []  groceries = new String [4] [5];
+
+		
+		groceries [0] [0]="cola";
+		groceries [0] [1]="ornage juice";
+		groceries [0] [2]="Lemon";
+		groceries [0] [3]="Green tea";
+		groceries [0] [4]="Balck cofee";
+
+		groceries[1][0]="popcorn";
+		groceries[1][1]="Trail mix";
+		groceries[1][2]="beef jerky";
+		groceries[1][3]="salted nuts";
+	    groceries[1][4]="rice cake";
+	    
+	    groceries[2][0]="cottage cheese";
+		groceries[2][1]="Heavy cream";
+		groceries[2][2]="Ice cream";
+		groceries[2][3]="parmesan cheese";
+	    groceries[2][4]="Eggnog";
+	    
+	    groceries[3][0]="Apple";
+		groceries[3][1]="Bananas";
+		groceries[3][2]="carrot";
+		groceries[3][3]="Potatoes";
+	    groceries[3][4]="onion";
+
+
+
+
+	}
+
+}
