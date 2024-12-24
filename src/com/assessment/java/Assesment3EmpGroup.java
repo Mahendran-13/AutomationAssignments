586af8cb44fb05cd0db63a015bef4d8c2c49fce0
Mahendran <mahendranvsm13@gmail.com>
2024-12-24 19:09:36 +0530

    Assesment3

--- src/com/assessment/java/Assesment3EmpGroup.java
+++ src/com/assessment/java/Assesment3EmpGroup.java
@@ -8,9 +8,19 @@ public class Assesment3EmpGroup {
 		
 		Assesment3Employee obj = new Assesment3Employee();
 		
-		System.out.println(obj.empname);
-		System.out.println(obj.empid);
-
+		String [] ename = new String [3];
+		int [] eid= new int [3];
+		
+		ename[0]= obj.empname[0];
+		ename[1]= obj.empname[1];
+		ename[2]= obj.empname[2];
+		eid[0]= obj.empid[0];
+		eid[1]= obj.empid[1];
+		eid[2]= obj.empid[2];
+		System.out.println("Employee name :"+ename[0]+" Employee id :"+eid[0]);
+		System.out.println("Employee name :"+ename[1]+" Employee id :"+eid[1]);	
+		System.out.println("Employee name :"+ename[2]+" Employee id :"+eid[2]);
+		
 	}
 
 
