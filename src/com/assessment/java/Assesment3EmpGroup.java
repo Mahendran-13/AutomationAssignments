bfa1cfb7d0780ff62bb47a2326173ca90ea7c070
Mahendran <mahendranvsm13@gmail.com>
2024-12-24 07:28:43 +0530

    new changes

---
+++ src/com/assessment/java/Assesment3EmpGroup.java
@@ -0,0 +1,19 @@
+package com.assessment.java;
+
+public class Assesment3EmpGroup {
+
+
+
+	public static void main(String[] args) {
+		
+		Assesment3Employee obj = new Assesment3Employee();
+		
+		System.out.println(obj.empname);
+		System.out.println(obj.empid);
+
+	}
+
+
+
+
+}
---
+++ src/com/assessment/java/Assesment3Employee.java
@@ -0,0 +1,12 @@
+package com.assessment.java;
+
+public class Assesment3Employee {
+
+	String[] empname= {" Bharth", "ABC", "XYZ"};
+	int[]  empid = {1234, 1235,1236};
+
+
+
+}
+
+
