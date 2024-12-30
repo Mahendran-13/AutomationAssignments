bc78b2edbe099d34b64d4d89a902255d1aa20f5c
Mahendran <mahendranvsm13@gmail.com>
2024-12-30 20:06:41 +0530

    Data type changes

--- src/com/assessment/java/DatatypeAssementday1.java
+++ src/com/assessment/java/DatatypeAssementday1.java
@@ -12,37 +12,81 @@ public class DatatypeAssementday1 {
 
 		//data types
 		float Temperature = 25.5f;
-		boolean CustomerOrder = false;
-		int PhnoneNumber = 123 - 456 - 7890;
-		float CustomerOrder1 = 1000.50f;
-		String emailAddress = "john.doe@example.com";
-		boolean MaritalStatus = false;
-		String PersonOccupation = "Software Enginner";
-		String FavouriteColor = "Blue";
-		int year = 2024;
-		int followers = 100000;
-		float MovieRating = 7.5f;
-		char BloodType = 'B';
-		String BookTitle = "To kill a Mockingbird";
-		short NoOfEmployee = 500;
-		String EventTime= "2.30 PM";
-		String CountryName = "United States";
-		String PersonEyeColor = "Brown";
-		String PersonBirthPlace = "New york city";
-		float DBTwoCities = 200.5f;
-
-		// print statement
 		System.out.print("Temperature of city is :"+ Temperature);
+
+	
+		boolean CustomerOrder = false;
 		System.out.println();
 		System.out.println("Did customer has placed the order:"+ CustomerOrder);
+		
+		
+		int PhnoneNumber = 123 - 456 - 7890;
 		System.out.println("User phone number is :"+ PhnoneNumber);
-
+		
+		
+		float CustomerOrder1 = 1000.50f;
+		System.out.println("The customer order is "+CustomerOrder1);
+		
+		
+		String emailAddress = "john.doe@example.com";
 		System.out.println("The person email id is:"+ emailAddress);
-		System.out.println("The person occopation is:"+ PersonOccupation);
-		System.out.printf("The person favouite color %s", "Blue");
-		System.out.println();
-		System.out.printf("the current year is %d", year);
+		
+		
+		boolean MaritalStatus = false;
+		System.out.println("The person marital status is:"+ MaritalStatus);
 
+		String PersonOccupation = "Software Enginner";
+		System.out.println("The person occopation is:"+ PersonOccupation);
+
+		
+		String FavouriteColor = "Blue";
+		System.out.println("My favouite color is"+FavouriteColor);
+		
+		int year = 2024;
+		System.out.println("the current year is "+year);
+		int followers = 100000;
+		
+		
+		float MovieRating = 7.5f;
+		System.out.println("Movie rating is "+MovieRating);
+		
+		
+		char BloodType = 'B';
+		System.out.println("My Boold type is "+BloodType);
+		
+		
+		String BookTitle = "To kill a Mockingbird";
+		System.out.println("My favourite book is "+BookTitle);
+		
+		
+		short NoOfEmployee = 500;
+		System.out.println("Total no of employee is "+NoOfEmployee);
+		
+		String EventTime= "2.30 PM";
+		System.out.println("the event time is"+EventTime);
+		
+		
+		String CountryName = "United States";
+		System.out.println("My favouitre country is "+CountryName);
+		
+		
+		String PersonEyeColor = "Brown";
+		System.out.println("My eye color is "+PersonEyeColor);
+		
+		String PersonBirthPlace = "New york city";
+		System.out.println("My birth place is "+PersonBirthPlace);
+		
+		
+		float DBTwoCities = 200.5f;
+		
+
+		// print statement
+		
+		
+
+		
+		
+		
 	}
 
 }
